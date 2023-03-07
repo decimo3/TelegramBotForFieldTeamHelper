83ad7b6bee6b03db691d217f87371e2dae38ebaf
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-03-07 10:56:12 -0300

    working with multiple line outputs

--- Temporary.cs
+++ Temporary.cs
@@ -3,24 +3,24 @@ public static class Temporary
 {
   private static string PYTHON_PATH = @"C:\Users\ruan.camello\scoop\apps\python\current\python.exe";
   private static string PYTHON_FILE = @"C:\Users\ruan.camello\Documents\Development\Automacao\sap.py";
-  public static string executar(string aplicacao, string informacao)
+  private static string EXCEL_FILE = @"C:\Users\ruan.camello\Documents\Development\Automacao\xls.py";
+  public static List<string> executar(string aplicacao, string informacao)
   {
     using(var proc = new System.Diagnostics.Process{
       StartInfo = new System.Diagnostics.ProcessStartInfo
         {
           FileName = PYTHON_PATH,
-          Arguments = $"{PYTHON_FILE} {aplicacao.ToLower()} {informacao.ToLower()}",
+          Arguments = $"{PYTHON_FILE} {aplicacao.ToLower()} {informacao}",
           UseShellExecute = false,
           RedirectStandardOutput = true,
           CreateNoWindow = true
         }})
       {
-        string? linha = "";
+        var linha = new List<string>();
         proc.Start();
         while (!proc.StandardOutput.EndOfStream)
         {
-          linha = proc.StandardOutput.ReadLine();
-          // do something with line
+          linha.Add(proc.StandardOutput.ReadLine()!);
         }
         return linha;
       }
