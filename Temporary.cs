5428208fc67f1e4cb90ecf4a834e1809cd2ef1fe
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-03-17 11:20:15 -0300

    FIX: added loop to wait for processing to finish

--- Temporary.cs
+++ Temporary.cs
@@ -2,15 +2,15 @@ namespace telbot;
 public static class Temporary
 {
   private static string PYTHON_PATH = @"C:\Users\ruan.camello\scoop\apps\python\current\python.exe";
-  private static string PYTHON_FILE = @"C:\Users\ruan.camello\Documents\Development\Automacao\sap.py";
-  private static string EXCEL_FILE = @"C:\Users\ruan.camello\Documents\Development\Automacao\xls.py";
+  private static string SAP_SCRIPT = @"C:\Users\ruan.camello\Documents\Development\Automacao\src\sap.py";
+  private static string IMG_SCRIPT = @"C:\Users\ruan.camello\Documents\Development\Automacao\src\img.py";
   public static List<string> executar(string aplicacao, string informacao)
   {
     using(var proc = new System.Diagnostics.Process{
       StartInfo = new System.Diagnostics.ProcessStartInfo
         {
           FileName = PYTHON_PATH,
-          Arguments = $"{PYTHON_FILE} {aplicacao.ToLower()} {informacao}",
+          Arguments = $"{SAP_SCRIPT} {aplicacao} {informacao}",
           UseShellExecute = false,
           RedirectStandardOutput = true,
           CreateNoWindow = true
@@ -25,20 +25,26 @@ public static class Temporary
         return linha;
       }
   }
-  public static void executar(List<string> listaValoresSeparadosPorTabulacao)
+  public static List<string> executar(List<string> listaValoresSeparadosPorTabulacao)
   {
-    string valoresSeparadosPorTabulacao = string.Join("\n", listaValoresSeparadosPorTabulacao.ToArray());
+    string textoValoresSeparadosPorTabulacao = string.Join("\n", listaValoresSeparadosPorTabulacao);
     using(var proc = new System.Diagnostics.Process{
       StartInfo = new System.Diagnostics.ProcessStartInfo
         {
           FileName = PYTHON_PATH,
-          Arguments = $"{EXCEL_FILE} {valoresSeparadosPorTabulacao}",
+          Arguments = $"{IMG_SCRIPT} \"{textoValoresSeparadosPorTabulacao}\"",
           UseShellExecute = false,
           RedirectStandardOutput = true,
           CreateNoWindow = true
         }})
       {
+        var linha = new List<string>();
         proc.Start();
+        while (!proc.StandardOutput.EndOfStream)
+        {
+          linha.Add(proc.StandardOutput.ReadLine()!);
+        }
+        return linha;
       }
   }
 }
