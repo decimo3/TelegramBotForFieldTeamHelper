c137324e0528016b26347553ea30cb47e5bb8c2a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-03-18 09:33:06 -0300

    FIX: nullable string and reduce roles

--- Users.cs
+++ Users.cs
@@ -1,9 +1,9 @@
 namespace telbot;
 public class Users
 {
-  public string name {get; set;}
+  public string? name {get; set;}
   public long Id {get; set;}
   public DateTime exp {get; set;}
   public Role role { get; set;}
-  public enum Role {Administrador, Supervisor, Eletricista}
+  public enum Role {Eletricista, Administrativo}
 }
