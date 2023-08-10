a3e47a745e37180307668914df60cc9084487a3f
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-08-10 07:58:25 -0300

    Send empty string when request argument object is null

--- Handles/HandleMessage.cs
+++ Handles/HandleMessage.cs
@@ -32,7 +32,8 @@ public class HandleMessage
     if(SAPerrorMessage is not null) await sendTextMesssageWraper(id, SAPerrorMessage);
     await sendTextMesssageWraper(id, "Não foi possível processar a sua solicitação!");
     await sendTextMesssageWraper(id, "Solicite a informação para o monitor(a)");
-    Database.inserirRelatorio(new logsModel(id, request.aplicacao, request.informacao, false));
+    if(request is null) Database.inserirRelatorio(new logsModel(id, string.Empty, string.Empty, false));
+    else Database.inserirRelatorio(new logsModel(id, request.aplicacao, request.informacao, false));
     return;
   }
   public async Task SendPhotoAsyncWraper(long id, Stream stream)
