4d73d71b54b92270cfaaf5bd1c3154b87d4ec876
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-09-03 22:18:13 -0300

    Moved protection validation to Configuration class

--- Startup.cs
+++ Startup.cs
@@ -9,28 +9,9 @@ class Startup
     Console.WriteLine("# Author: decimo3 (github.com/decimo3)      #");
     Console.WriteLine("# Repository: TelegramBotForFieldTeamHelper #");
     Console.WriteLine("#############################################");
-    var agora = DateTime.Now;
-    var prazo = new DateTime(year: 2023, month: 9, day: 1);
-    if(agora > prazo)
-    {
-      Console.BackgroundColor = ConsoleColor.Red;
-      Console.Beep();
-      Console.Write("O período de licença de uso expirou!");
-      Console.BackgroundColor = ConsoleColor.Black;
-      return;
-    }
     if(args.Contains("--em-desenvolvimento")) DotEnv.Load();
     if(System.Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") == "Development") DotEnv.Load();
     var config = new Configuration(args);
-    Console.WriteLine(config.HOSTNAME);
-    if(config.HOSTNAME != "PAT033630")
-    {
-      Console.BackgroundColor = ConsoleColor.Red;
-      Console.Beep();
-      Console.Write("A licença de uso não permite o uso em outra máquina!");
-      Console.BackgroundColor = ConsoleColor.Black;
-      return;
-    }
     Database.configurarBanco(config);
     var program = new Program(config);
   }
