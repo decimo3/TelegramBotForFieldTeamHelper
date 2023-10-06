7601590c73629bc062f72e0045935b67bff4762c
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-10-06 13:57:26 -0300

    Moved passive application check before checks for similar applications.
    The verification as it was, was never carried out,
    because similar applications were checked
    and returned before it was checked

--- Handles/HandleInformation.cs
+++ Handles/HandleInformation.cs
@@ -19,6 +19,13 @@ public class HandleInformation
   }
   async private Task<bool> has_impediment()
   {
+    if(request.aplicacao == "passivo" && (DateTime.Today.DayOfWeek == DayOfWeek.Friday || DateTime.Today.DayOfWeek == DayOfWeek.Saturday))
+    {
+      await bot.sendTextMesssageWraper(user.id, "Essa aplicação não deve ser usada na sexta e no sábado!");
+      await bot.sendTextMesssageWraper(user.id, "Notas de recorte devem ter todas as faturas cobradas!");
+      Database.inserirRelatorio(new logsModel(user.id, request.aplicacao, request.informacao, false, request.received_at));
+      return true;
+    }
     // Knockout system to mitigate the queue
     if((request.tipo == TypeRequest.pdfInfo) && (cfg.GERAR_FATURAS == true))
     {
@@ -36,13 +43,6 @@ public class HandleInformation
       await bot.sendTextMesssageWraper(user.id, "O sistema SAP não está gerando faturas no momento!\nEstaremos te enviando as informações do cliente!");
       request.aplicacao = "informacao";
       return false;
-    }
-    if(request.aplicacao == "passivo" && (DateTime.Today.DayOfWeek == DayOfWeek.Friday || DateTime.Today.DayOfWeek == DayOfWeek.Saturday))
-    {
-      await bot.sendTextMesssageWraper(user.id, "Essa aplicação não deve ser usada na sexta e no sábado!");
-      await bot.sendTextMesssageWraper(user.id, "Notas de recorte devem ter todas as faturas cobradas!");
-      Database.inserirRelatorio(new logsModel(user.id, request.aplicacao, request.informacao, false, request.received_at));
-      return true;
     }
     if((request.tipo == TypeRequest.xlsInfo) && (user.has_privilege == false))
     {
