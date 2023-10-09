f91403933714bb2390009a6afbe67d8cb2030fb3
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-10-09 14:43:17 -0300

    Added ability to expire SAP requests

--- Helpers/Temporary.cs
+++ Helpers/Temporary.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 namespace telbot;
 public static class Temporary
 {
@@ -5,24 +6,47 @@ public static class Temporary
   {
     var argumentos = $"{aplicacao} {informacao} {cfg.INSTANCIA}";
     if(cfg.SAP_RESTRITO) argumentos += " --sap-restrito";
-    using(var proc = new System.Diagnostics.Process{
-      StartInfo = new System.Diagnostics.ProcessStartInfo
-        {
-          FileName = cfg.SAP_SCRIPT,
-          Arguments = argumentos,
-          UseShellExecute = false,
-          RedirectStandardOutput = true,
-          CreateNoWindow = true
-        }})
+    var proc = new System.Diagnostics.Process();
+    var startInfo = new System.Diagnostics.ProcessStartInfo
       {
-        var linha = new List<string>();
-        proc.Start();
-        while (!proc.StandardOutput.EndOfStream)
+        FileName = cfg.SAP_SCRIPT,
+        Arguments = argumentos,
+        UseShellExecute = false,
+        RedirectStandardOutput = true,
+        CreateNoWindow = true
+      };
+    proc.StartInfo = startInfo;
+    var linha = new List<string>();
+    proc.Start();
+    var tempo = new System.Threading.Timer(state => {
+      if(!proc.HasExited)
+      {
+        linha.Insert(0,"ERRO: O SAP demorou muito tempo na solicitação e foi encerrado!");
+        var mos = Temporary.GetChildProcesses(proc);
+        foreach (var mo in mos)
         {
-          linha.Add(proc.StandardOutput.ReadLine()!);
+          mo.Kill();
         }
-        return linha;
       }
+    }, null, 60_000, Timeout.Infinite);
+    while (!proc.StandardOutput.EndOfStream)
+    {
+      linha.Add(proc.StandardOutput.ReadLine()!);
+    }
+    tempo.Dispose();
+    proc.Dispose();
+    return linha;
+  }
+  public static IEnumerable<Process> GetChildProcesses(this Process process)
+  {
+    var children = new List<Process>();
+    var queryProcess = $"Select * From Win32_Process Where ParentProcessID={process.Id}";
+    var mos = new System.Management.ManagementObjectSearcher(queryProcess);
+    foreach (var mo in mos.Get())
+    {
+        children.Add(Process.GetProcessById(Convert.ToInt32(mo["ProcessID"])));
+    }
+    return children;
   }
   public static List<string> executar(Configuration cfg, List<string> listaValoresSeparadosPorTabulacao)
   {
