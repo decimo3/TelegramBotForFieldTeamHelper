deef2c2819bfeca2618c0efa90640b1b04224089
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-10-09 14:34:39 -0300

    Added description of the difference between 'leiturista' and 'roteiro'

--- Handles/HandleCommand.cs
+++ Handles/HandleCommand.cs
@@ -25,7 +25,8 @@ public class HandleCommand
         await bot.sendTextMesssageWraper(user.id, "Digite o tipo de informação que deseja e depois o número da nota ou instalação.");
         await bot.sendTextMesssageWraper(user.id, "*TELEFONE* ou *CONTATO* para receber todos os telefones no cadastro do cliente;");
         await bot.sendTextMesssageWraper(user.id, "*COORDENADA* para receber um link da localização no cadastro do cliente;");
-        await bot.sendTextMesssageWraper(user.id, "*LEITURISTA* ou *ROTEIRO* para receber a lista de instalações ordenada por horário;");
+        await bot.sendTextMesssageWraper(user.id, "*ROTEIRO* para receber a lista de instalações ordenada por horário;");
+        await bot.sendTextMesssageWraper(user.id, "*LEITURISTA* para receber a lista de instalações ordenada por sequencia;");
         await bot.sendTextMesssageWraper(user.id, "*PENDENTE* para receber a lista de débitos para aquela instalação do cliente;");
         await bot.sendTextMesssageWraper(user.id, "*FATURA* ou *DEBITO* _(sem acentuação)_ para receber as faturas vencidas em PDF (limite de 5 faturas)");
         await bot.sendTextMesssageWraper(user.id, "*HISTORICO* _(sem acentuação)_ para receber a lista com os 5 últimos serviços para a instalação;");
