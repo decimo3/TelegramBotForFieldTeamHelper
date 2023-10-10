de19326aacdfcdc1a499316721c9881d92908fd4
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-10-10 14:10:24 -0300

    REFACTORED: Message suited to the console output standard

--- Helpers/WakeUpSAP.cs
+++ Helpers/WakeUpSAP.cs
@@ -4,35 +4,33 @@ public class WakeUpSAP
   private readonly Int32 tempo = 5 * 60 * 1_000;
   public WakeUpSAP(Configuration cfg)
   {
-    Console.WriteLine("> Manager: Iniciado sistema de despertador do SapAutomation!");
+    Console.WriteLine($"< {DateTime.Now} Manager: Iniciado sistema de despertador do SapAutomation!");
     while(true)
     {
       var horario = System.IO.File.GetLastWriteTime("database.db");
       var prazo = DateTime.Now.AddMinutes(-5);
       var diferenca = horario - prazo;
-      Console.WriteLine($"> Manager: Última solicitação registrada às {horario}.");
+      Console.WriteLine($"< {DateTime.Now} Manager: Última solicitação registrada às {horario}.");
       if(diferenca.TotalMinutes < 0)
       {
-        Console.WriteLine("> Manager: Realizando consulta para manter o SAP acordado...");
+        Console.WriteLine($"< {DateTime.Now} Manager: Realizando consulta para manter o SAP acordado...");
           var resposta = Temporary.executar(cfg, "desperta", "1380763967");
           if(resposta[0].StartsWith("ERRO"))
           {
             Console.Beep();
             Console.BackgroundColor = ConsoleColor.Red;
-            Console.WriteLine("> Manager: A solicitação não pode ser concluída!");
+            Console.WriteLine($"< {DateTime.Now} Manager: A solicitação não pode ser concluída!");
             Console.ResetColor();
-            Console.WriteLine("> Manager: Tentando novamente em daqui a 1 minuto...");
+            Console.WriteLine($"< {DateTime.Now} Manager: Tentando novamente em daqui a 5 minutos...");
             Database.inserirRelatorio(new logsModel(0,"desperta", "0", false, DateTime.Now));
-            this.tempo = 1 * 60 * 1_000;
           }
           else
           {
-            Console.WriteLine("> Manager: Consulta fantasma realizada com sucesso!");
+            Console.WriteLine($"< {DateTime.Now} Manager: Consulta fantasma realizada com sucesso!");
             Database.inserirRelatorio(new logsModel(0,"desperta", "0", true, DateTime.Now));
-            this.tempo = 5 * 60 * 1_000;
           }
         }
-      Console.WriteLine($"> Manager: Última verificação realizada às {DateTime.Now}.");
+      Console.WriteLine($"< {DateTime.Now} Manager: Última verificação foi realizada agora mesmo.");
       System.Threading.Thread.Sleep(tempo);
       }
     }
