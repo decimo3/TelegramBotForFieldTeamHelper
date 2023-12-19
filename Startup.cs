a6dd67c0cb41d91cf708a761b17b81796035919c
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-12-19 09:41:05 -0300

    Test updater check

--- Startup.cs
+++ Startup.cs
@@ -3,7 +3,7 @@ using dotenv.net;
 namespace telbot;
 class Startup
 {
-  public static void Main(string[] args)
+  public static async Task Main(string[] args)
   {
     Console.WriteLine("#############################################");
     Console.WriteLine("# BOT de atendimento automágico MestreRuan  #");
@@ -13,11 +13,17 @@ class Startup
     if(args.Contains("--em-desenvolvimento")) DotEnv.Load();
     if(System.Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") == "Development") DotEnv.Load();
     var config = new Configuration(args);
+    if(!config.IS_DEVELOPMENT)
+    {
+      await Updater.CheckUpdates();
+    }
+    /*
     Database.configurarBanco(config);
     var wakeup = new WakeUpSAP(config);
     Task task = Task.Factory.StartNew(() => {
       wakeup.Start();
     });
     var program = new Program(config);
+    */
   }
 }
