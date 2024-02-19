4f1af8fd102c0f381aa95f885d53a90fe3f03160
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-02-19 10:47:53 -0300

    Removed obsolete system, replaced with SAP FrontEnd self-login system

--- Startup.cs
+++ Startup.cs
@@ -14,10 +14,6 @@ class Startup
     if(System.Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") == "Development") DotEnv.Load();
     var config = new Configuration(args);
     Database.configurarBanco(config);
-    var wakeup = new WakeUpSAP(config);
-    Task task = Task.Factory.StartNew(() => {
-      wakeup.Start();
-    });
     var program = new Program(config);
   }
 }
