1c9a663b9519ec91418592810e8c58088ce0e93a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-03-02 19:17:43 -0300

    Created model to store lost message information

---
+++ Models/errorReport.cs
@@ -0,0 +1,7 @@
+namespace telbot;
+public class errorReport
+{
+  public Int64 identificador { get; set; } = 0;
+  public String mensagem { get; set; } = String.Empty;
+  public Stream binario { get; set; } = Stream.Null;
+}
