edd0e6fa5de857aea29aa1af9843446ee6b2187e
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-03-07 10:00:55 -0300

    1. Added wait for LOCKFILE check; 2. Fixed return without deleting the LOCKFILE; 3. Corrected file name sent to the user; 4. Corrected creation of LOCKFILE before checking the existence of the statement;

--- Handles/HandleAnnouncement.cs
+++ Handles/HandleAnnouncement.cs
@@ -3,17 +3,36 @@ public static class HandleAnnouncement
 {
   public static async void Vencimento(HandleMessage msg, Configuration cfg)
   {
+    while(true)
+    {
+      if(!System.IO.File.Exists(cfg.LOCKFILE)) break;
+      else System.Threading.Thread.Sleep(1_000);
+    }
+    Console.WriteLine($"< {DateTime.Now} Manager: ForAllAnnouncement - Vencimentos");
     System.IO.File.Create(cfg.LOCKFILE).Close();
     var relatorio_resultado = Temporary.executar(cfg, "vencimento", "7");
     var relatorio_arquivo = cfg.CURRENT_PATH + "\\tmp\\temporario.csv";
-    if(!System.IO.File.Exists(relatorio_arquivo)) return;
-    if(!relatorio_resultado.Any()) return;
-    if(relatorio_resultado.First().StartsWith("ERRO:")) return;
+    if(!System.IO.File.Exists(relatorio_arquivo))
+    {
+      System.IO.File.Delete(cfg.LOCKFILE);
+      return;
+    }
+    if(!relatorio_resultado.Any())
+    {
+      System.IO.File.Delete(cfg.LOCKFILE);
+      return;
+    }
+    if(relatorio_resultado.First().StartsWith("ERRO:"))
+    {
+      System.IO.File.Delete(cfg.LOCKFILE);
+      return;
+    }
     var relatorio_mensagem = String.Join('\n', relatorio_resultado);
     var padrao = @"([0-9]{2})/([0-9]{2})/([0-9]{4}) ([0-9]{2}):([0-9]{2}):([0-9]{2})";
     var relatorio_filename = new System.Text.RegularExpressions.Regex(padrao).Match(relatorio_mensagem).Value;
+    var padrao_trocar = @"$3-$2-$1_$4-$5-$6\.csv";
+    relatorio_filename = new System.Text.RegularExpressions.Regex(padrao).Replace(relatorio_filename, padrao_trocar);
     Stream relatorio_stream = System.IO.File.OpenRead(relatorio_arquivo);
-    Console.WriteLine($"< {DateTime.Now} Manager: ForAllAnnouncement");
     var usuarios = Database.recuperarUsuario();
     var tasks = new List<Task>();
     foreach (var usuario in usuarios)
@@ -32,9 +51,9 @@ public static class HandleAnnouncement
   }
   public static async void Comunicado(HandleMessage msg, Configuration cfg)
   {
-    System.IO.File.Create(cfg.LOCKFILE).Close();
     var comunicado_arquivo = cfg.CURRENT_PATH + "\\comunicado.txt";
     if(!System.IO.File.Exists(comunicado_arquivo)) return;
+    System.IO.File.Create(cfg.LOCKFILE).Close();
     var comunicado_mensagem = System.IO.File.ReadAllText(comunicado_arquivo);
     Console.WriteLine($"< {DateTime.Now} Manager: Comunicado para todos:");
     var usuarios = Database.recuperarUsuario();
