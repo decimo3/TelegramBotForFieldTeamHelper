beaeeac0294b01c933469acf436067ce548d1cfa
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-03-09 19:07:25 -0300

    1. Added retry every five minutes if the expiration check fails; 2. Added empty file check before sending;

--- Handles/HandleAnnouncement.cs
+++ Handles/HandleAnnouncement.cs
@@ -1,38 +1,55 @@
 namespace telbot.handle;
 public static class HandleAnnouncement
 {
+  private static int CINCO_MINUTOS = 1_000 * 60 * 5;
   public static async void Vencimento(HandleMessage msg, Configuration cfg)
   {
+    while(true)
+    {
     while(true)
     {
       if(!System.IO.File.Exists(cfg.LOCKFILE)) break;
       else System.Threading.Thread.Sleep(1_000);
     }
-    Console.WriteLine($"< {DateTime.Now} Manager: ForAllAnnouncement - Vencimentos");
+    Console.WriteLine($"< {DateTime.Now} Manager: Comunicado para todos - Vencimentos");
     System.IO.File.Create(cfg.LOCKFILE).Close();
     var relatorio_resultado = Temporary.executar(cfg, "vencimento", "7");
-    var relatorio_arquivo = cfg.CURRENT_PATH + "\\tmp\\temporario.csv";
-    if(!System.IO.File.Exists(relatorio_arquivo))
-    {
-      System.IO.File.Delete(cfg.LOCKFILE);
-      return;
-    }
+    var relatorio_caminho = cfg.CURRENT_PATH + "\\tmp\\temporario.csv";
     if(!relatorio_resultado.Any())
     {
+      Temporary.ConsoleWriteError("Erro ao gerar o relatório de notas em aberto!\nTentaremos novamente daqui a cinco minutos");
       System.IO.File.Delete(cfg.LOCKFILE);
-      return;
+      System.Threading.Thread.Sleep(CINCO_MINUTOS);
+      continue;
     }
     if(relatorio_resultado.First().StartsWith("ERRO:"))
     {
+      Temporary.ConsoleWriteError("Erro ao gerar o relatório de notas em aberto!\nTentaremos novamente daqui a cinco minutos");
       System.IO.File.Delete(cfg.LOCKFILE);
-      return;
+      System.Threading.Thread.Sleep(CINCO_MINUTOS);
+      continue;
+    }
+    if(!System.IO.File.Exists(relatorio_caminho))
+    {
+      Temporary.ConsoleWriteError("Erro ao gerar o relatório de notas em aberto!\nTentaremos novamente daqui a cinco minutos");
+      System.IO.File.Delete(cfg.LOCKFILE);
+      System.Threading.Thread.Sleep(CINCO_MINUTOS);
+      continue;
+    }
+    Stream relatorio_arquivo = System.IO.File.OpenRead(relatorio_caminho);
+    if(relatorio_arquivo.Length == 0)
+    {
+      relatorio_arquivo.Close();
+      Temporary.ConsoleWriteError("Erro ao gerar o relatório de notas em aberto!\nTentaremos novamente daqui a cinco minutos");
+      System.IO.File.Delete(cfg.LOCKFILE);
+      System.Threading.Thread.Sleep(CINCO_MINUTOS);
+      continue;
     }
     var relatorio_mensagem = String.Join('\n', relatorio_resultado);
     var padrao = @"([0-9]{2})/([0-9]{2})/([0-9]{4}) ([0-9]{2}):([0-9]{2}):([0-9]{2})";
     var relatorio_filename = new System.Text.RegularExpressions.Regex(padrao).Match(relatorio_mensagem).Value;
     var padrao_trocar = @"$3-$2-$1_$4-$5-$6\.csv";
     relatorio_filename = new System.Text.RegularExpressions.Regex(padrao).Replace(relatorio_filename, padrao_trocar);
-    Stream relatorio_stream = System.IO.File.OpenRead(relatorio_arquivo);
     var usuarios = Database.recuperarUsuario();
     var tasks = new List<Task>();
     foreach (var usuario in usuarios)
@@ -42,14 +59,16 @@ public static class HandleAnnouncement
       tasks.Add(msg.sendTextMesssageWraper(usuario.id, relatorio_mensagem, true, false));
       if(usuario.has_privilege)
       {
-        relatorio_stream.Position = 0;
-        tasks.Add(msg.SendDocumentAsyncWraper(usuario.id, relatorio_stream, relatorio_filename));
+        relatorio_arquivo.Position = 0;
+        tasks.Add(msg.SendDocumentAsyncWraper(usuario.id, relatorio_arquivo, relatorio_filename));
       }
     }
     await Task.WhenAll(tasks);
-    relatorio_stream.Close();
-    System.IO.File.Delete(relatorio_arquivo);
+    relatorio_arquivo.Close();
+    System.IO.File.Delete(relatorio_caminho);
     System.IO.File.Delete(cfg.LOCKFILE);
+    break;
+    }
   }
   public static async void Comunicado(HandleMessage msg, Configuration cfg)
   {
@@ -66,13 +85,16 @@ public static class HandleAnnouncement
     System.IO.File.Create(cfg.LOCKFILE).Close();
     
     var comunicado_mensagem = has_txt ? File.ReadAllText(mensagem_caminho) : String.Empty;
+    if(comunicado_mensagem.Length == 0) has_txt = false;
     Stream comunicado_imagem = has_jpg ? File.OpenRead(imagem_caminho) : Stream.Null;
+    if(comunicado_imagem.Length == 0) has_jpg = false;
     Stream comunicado_video = has_mp4 ? File.OpenRead(videoclipe_caminho) : Stream.Null;
+    if(comunicado_video.Length == 0) has_mp4 = false;
     
     var photo_id = has_jpg ? await msg.SendPhotoAsyncWraper(cfg.ID_ADM_BOT, comunicado_imagem) : String.Empty;
     var video_id = has_mp4 ? await msg.SendVideoAsyncWraper(cfg.ID_ADM_BOT, comunicado_video) : String.Empty;
 
-    Console.WriteLine($"< {DateTime.Now} Manager: Comunicado para todos:");
+    Console.WriteLine($"< {DateTime.Now} Manager: Comunicado para todos - Comunicado");
     var usuarios = Database.recuperarUsuario();
     var tasks = new List<Task>();
     foreach (var usuario in usuarios)
