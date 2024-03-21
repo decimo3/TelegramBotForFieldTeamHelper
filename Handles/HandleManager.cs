7f869e130a92251fa1d4e1f39076a911d41a54c2
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-03-21 11:48:40 -0300

    Rewritten class: 1. Removed redundant methods; 2. Improved permission checking; 3. Pre-registration verification implemented; 4. Added function for changing the database.

--- Handles/HandleManager.cs
+++ Handles/HandleManager.cs
@@ -4,95 +4,76 @@ public static class Manager
 {
   public async static Task HandleManager(HandleMessage bot, Configuration cfg, UsersModel user, Request request)
   {
-    switch (request.aplicacao)
-    {
-      case "autorizar": await autorizar(bot, cfg, user, request); break;
-      case "atualizar": await autorizar(bot, cfg, user, request); break;
-      case "monitorador": await promover(bot, cfg, user, request); break;
-      case "comunicador": await promover(bot, cfg, user, request); break;
-      case "administrador": await promover(bot, cfg, user, request); break;
-      case "desautorizar": await promover(bot, cfg, user, request); break;
-      case "supervisor": await promover(bot, cfg, user, request); break;
-    }
-    return;
-  }
-  public async static Task autorizar(HandleMessage bot, Configuration cfg, UsersModel user, Request request)
-  {
     if(!user.pode_autorizar())
     {
       await bot.sendTextMesssageWraper(user.id, "Você não tem permissão para alterar usuários!");
-      Database.inserirRelatorio(new logsModel(user.id, request.aplicacao, request.informacao, false, request.received_at));
       return;
     }
     if(!Int64.TryParse(request.informacao, out long id))
     {
       await bot.sendTextMesssageWraper(user.id, "O identificador do usuário não é válido!");
-      Database.inserirRelatorio(new logsModel(user.id, request.aplicacao, request.informacao, false, request.received_at));
       return;
     }
-    if(Database.recuperarUsuario(id) is null)
+    var usuario = Database.recuperarUsuario(id);
+    if(usuario == null)
     {
-      try
-      {
-        Database.inserirUsuario(new UsersModel(id, user.id));
-        await bot.sendTextMesssageWraper(id, "Usuário autorizado com sucesso!");
-        await bot.sendTextMesssageWraper(user.id, "Usuário autorizado com sucesso!");
-        Database.inserirRelatorio(new logsModel(user.id, request.aplicacao, request.informacao, true, request.received_at));
-      }
-      catch
-      {
-        await bot.sendTextMesssageWraper(user.id, "Houve um problema em autorizar o usuário");
-        await bot.sendTextMesssageWraper(user.id, "Verifique as informações e tente novamente");
-        Database.inserirRelatorio(new logsModel(user.id, request.aplicacao, request.informacao, false, request.received_at));
-      }
-    }
-    else
-    {
-      if(Database.atualizarUsuario(id, user.id))
-      {
-        await bot.sendTextMesssageWraper(id, "Usuário atualizado com sucesso!");
-        await bot.sendTextMesssageWraper(user.id, "Usuário atualizado com sucesso!");
-        Database.inserirRelatorio(new logsModel(user.id, request.aplicacao, request.informacao, true, request.received_at));
-      }
-      else
-      {
-        await bot.sendTextMesssageWraper(user.id, "Houve um problema em atualizar o usuário");
-        await bot.sendTextMesssageWraper(user.id, "Verifique as informações e tente novamente");
-        Database.inserirRelatorio(new logsModel(user.id, request.aplicacao, request.informacao, false, request.received_at));
-      }
-    }
-  }
-  public async static Task promover(HandleMessage bot, Configuration cfg, UsersModel user, Request request)
-  {
-    if(request.aplicacao == "administrador" && user.has_privilege != UsersModel.userLevel.proprietario)
-    {
-      await bot.sendTextMesssageWraper(user.id, "Você não tem permissão definir administradores do sistema!");
-      Database.inserirRelatorio(new logsModel(user.id, request.aplicacao, request.informacao, false, request.received_at));
+      await bot.sendTextMesssageWraper(user.id, "Não há registro que esse usuário entrou em contato com o chatbot!");
       return;
     }
-    if(!user.pode_promover())
+    if(usuario.has_privilege == UsersModel.userLevel.proprietario)
     {
-      await bot.sendTextMesssageWraper(user.id, "Você não tem permissão para modificar usuários!");
-      Database.inserirRelatorio(new logsModel(user.id, request.aplicacao, request.informacao, false, request.received_at));
+      await bot.sendTextMesssageWraper(user.id, "Nenhum usuário tem permissão suficiente para alterar o proprietário!");
       return;
     }
-    if(!Int64.TryParse(request.informacao, out long id))
+    if(usuario.has_privilege == UsersModel.userLevel.administrador)
     {
-      await bot.sendTextMesssageWraper(user.id, "O identificador do usuário não é válido!");
-      Database.inserirRelatorio(new logsModel(user.id, request.aplicacao, request.informacao, false, request.received_at));
-      return;
+      if(!user.pode_promover())
+      {
+        await bot.sendTextMesssageWraper(user.id, "Você não tem permissão suficiente para alterar o administrador!");
+        return;
+      }
     }
-    if(Database.promoverUsuario(id, user.id, Enum.Parse<UsersModel.userLevel>(request.aplicacao)))
+    var cargo = Enum.Parse<UsersModel.userLevel>(request.aplicacao!);
+    usuario.has_privilege = cargo;
+    usuario.update_at = DateTime.Now;
+    switch (request.aplicacao)
     {
-      await bot.sendTextMesssageWraper(user.id, "Usuário alterado com sucesso!");
-      Database.inserirRelatorio(new logsModel(user.id, request.aplicacao, request.informacao, true, request.received_at));
-    }
-    else
-    {
-      await bot.sendTextMesssageWraper(user.id, "Houve um problema ao alterar o usuário");
-      await bot.sendTextMesssageWraper(user.id, "Verifique as informações e tente novamente");
-      Database.inserirRelatorio(new logsModel(user.id, request.aplicacao, request.informacao, false, request.received_at));
+      case "autorizar":
+      case "atualizar":
+      case "desautorizar":
+        await alterarUsuario(bot, user, usuario, request);
+      break;
+      case "monitorador":
+      case "comunicador":
+      case "supervisor":
+        if(!user.pode_promover())
+          await bot.sendTextMesssageWraper(user.id, "Você não tem permissão para alterar usuários!");
+        else
+          await alterarUsuario(bot, user, usuario, request);
+      break;
+      case "administrador":
+        if(user.has_privilege != UsersModel.userLevel.proprietario)
+          await bot.sendTextMesssageWraper(user.id, "Você não tem permissão para promover administradores!");
+        else
+          await alterarUsuario(bot, user, usuario, request);
+      break;
     }
     return;
+  }
+  private async static Task alterarUsuario(HandleMessage bot, UsersModel old_user, UsersModel new_user, Request request)
+  {
+    try
+    {
+      Database.alterarUsuario(new_user, old_user.id);
+      await bot.sendTextMesssageWraper(old_user.id, "Usuário atualizado com sucesso!");
+      await bot.sendTextMesssageWraper(new_user.id, "Usuário atualizado com sucesso!");
+      Database.inserirRelatorio(new logsModel(old_user.id, request.aplicacao, request.informacao, true, request.received_at));
+    }
+    catch
+    {
+      await bot.sendTextMesssageWraper(old_user.id, "Houve um problema em atualizar o usuário");
+      await bot.sendTextMesssageWraper(old_user.id, "Verifique as informações e tente novamente");
+      Database.inserirRelatorio(new logsModel(old_user.id, request.aplicacao, request.informacao, false, request.received_at));
+    }
   }
 }
