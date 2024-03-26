d8324f17932895332b96dd5bf79e7aa80bb0e7cb
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-03-26 12:13:59 -0300

    Changed local file password and added method to generate local tokens

--- Helpers/Authorization.cs
+++ Helpers/Authorization.cs
@@ -2,7 +2,7 @@ namespace telbot;
 using telbot.models;
 public static class Authorization
 {
-  private static string secret = "michael_jackson_da_silva";
+  private static string secret = "XDWUUQ5P9ZNZ97XI9VRIBNIHMPORBHRW";
   public static TokenModel? RecoveryToken (string token)
   {
     var DecodedToken = new JWT.Builder.JwtBuilder()
@@ -10,7 +10,17 @@ public static class Authorization
       .WithSecret(System.Text.Encoding.UTF8.GetBytes(secret))
       .MustVerifySignature()
       .Decode(token);
-    var tokemObject = System.Text.Json.JsonSerializer.Deserialize<TokenModel>(DecodedToken);
-    return tokemObject;
+    return System.Text.Json.JsonSerializer.Deserialize<TokenModel>(DecodedToken);
+  }
+  public static String? GenerateToken (TokenModel token)
+  {
+    var DecodedToken = new JWT.Builder.JwtBuilder()
+      .AddClaim("adm_id_bot", token.adm_id_bot)
+      .AddClaim("allowed_pc", token.allowed_pc)
+      .WithAlgorithm(new JWT.Algorithms.HMACSHA256Algorithm())
+      .WithSecret(System.Text.Encoding.UTF8.GetBytes(secret))
+      .MustVerifySignature()
+      .Encode();
+    return DecodedToken;
   }
 }
