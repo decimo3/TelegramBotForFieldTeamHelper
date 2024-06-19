2742e4288df4e6aad2e49ebc35382bcbeda29c64
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-06-19 12:57:06 -0300

    WIP

--- Helpers/Postgresql.cs
+++ Helpers/Postgresql.cs
@@ -1,7 +1,7 @@
 using telbot.Interfaces;
 namespace telbot.Helpers
 {
-  public sealed class PostgreSQL
+  public sealed class PostgreSQL : IDatabase
   {
     private static readonly PostgreSQL instance = new PostgreSQL();
     private Npgsql.NpgsqlConnection connection;
@@ -18,8 +18,7 @@ namespace telbot.Helpers
       if(dbuser is null) throw new InvalidOperationException("Environment variable PGUSER is not set!");
       var dbpass = System.Environment.GetEnvironmentVariable("PGPASSWORD");
       if(dbpass is null) throw new InvalidOperationException("Environment variable PGPASSWORD is not set!");
-      var dbbase = System.Environment.GetEnvironmentVariable("PGDATABASE");
-      if(dbbase is null) throw new InvalidOperationException("Environment variable PGDATABASE is not set!");
+      var dbbase = "chatbot";
       var stringconnection = new Npgsql.NpgsqlConnectionStringBuilder()
       {
         Host = dbhost,
@@ -33,5 +32,32 @@ namespace telbot.Helpers
     }
     public static PostgreSQL Instance { get { return instance; } }
     public Npgsql.NpgsqlConnection GetConnection() { return connection; }
+    public void CloseConnection()
+    {
+      if(connection != null && connection.State == System.Data.ConnectionState.Open)
+      {
+        connection.Close();
+      }
+    }
+    public void inserirUsuario(UsersModel user, Int64 inserted_by) 
+    {
+      var sql = @"INSERT INTO usersModel (id, create_at, update_at, has_privilege, inserted_by, phone_number) VALUES ($1), ($2), ($3), ($4), ($5), ($6)";
+      using (var cmd = new Npgsql.NpgsqlCommand(sql, connection))
+      {
+        cmd.Parameters = {};
+        cmd.ExecuteNonQuery();
+      }
+    }
+    public void alterarUsuario(UsersModel user_model, Int64 inserted_by) {}
+    public UsersModel recuperarUsuario(long id) {}
+    public List<UsersModel> recuperarUsuario() {}
+    public List<UsersModel> recuperarUsuario(Expression<Func<UsersModel, bool>> expression) {}
+    public void inserirRelatorio(logsModel logs) {}
+    public logsModel recuperarRelatorio(String aplicacao, Int64 informacao) {}
+    public List<logsModel> recuperarRelatorio() {}
+    public List<logsModel> recuperarRelatorio(Expression<Func<logsModel, bool>> expression) {}
+    public void InserirPerdido(errorReport report) {}
+    public List<errorReport> recuperarPerdido() {}
+    public void excluirPerdidos() {}
   }
 }
