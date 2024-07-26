f0f3c408e9e85418a5e3c0477516a62f329e092a
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-07-26 12:44:20 -0300

    Added class to be implemented to support asynchronous mode

---
+++ Handles/HandleAsynchronous.cs
@@ -0,0 +1,11 @@
+using telbot.Helpers;
+namespace telbot.handle;
+public static class HandleAsynchronous
+{
+  // TODO - Recebe e registra no banco de dados
+  public static async void Soiree(HandleMessage msg, Configuration cfg) { throw new NotImplementedException(); }
+  // TODO - Coleta do banco de dados e realiza a solicitação
+  public static async void Cooker(HandleMessage msg, Configuration cfg) { throw new NotImplementedException(); }
+  // TODO - Coleta resposta e responde ao usuário
+  public static async void Waiter(HandleMessage msg, Configuration cfg) { throw new NotImplementedException(); }
+}
