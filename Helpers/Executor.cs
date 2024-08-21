8cb2c5d214b815dc92423e389bd42a82c5595f00
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-08-21 15:36:34 -0300

    Refactored class to generalize methods

--- Helpers/Executor.cs
+++ Helpers/Executor.cs
@@ -1,73 +1,9 @@
 using System.Diagnostics;
 using telbot.Helpers;
+using telbot.Services;
 namespace telbot;
-public static class Temporary
+public static class Executor
 {
-  public static List<String> executar(String aplicacao, String argumentos, Boolean expect_return)
-  {
-    var linhas = new List<String>();
-    var processo = new System.Diagnostics.Process();
-    processo.StartInfo = new System.Diagnostics.ProcessStartInfo
-      {
-        FileName = aplicacao,
-        Arguments = argumentos,
-        UseShellExecute = !expect_return,
-        RedirectStandardOutput = expect_return,
-        CreateNoWindow = true
-      };
-    ConsoleWrapper.Debug(Entidade.Executor, $"{aplicacao} {argumentos} {expect_return}");
-    processo.Start();
-    if(expect_return)
-    {
-      while (!processo.StandardOutput.EndOfStream)
-      {
-        var linha = processo.StandardOutput.ReadLine();
-        if(!String.IsNullOrEmpty(linha)) linhas.Add(linha);
-      }
-    }
-    ConsoleWrapper.Debug(Entidade.Executor, String.Join('\n', linhas));
-    return linhas;
-  }
-  public static List<string> executar(String aplicacao, long informacao, long telefone = 0, String? regional = null)
-  {
-    var cfg = Configuration.GetInstance();
-    var argumentos = $"{aplicacao} {informacao} {cfg.INSTANCIA}";
-    if(telefone != 0) argumentos += $" --telefone={telefone}";
-    if(cfg.SAP_RESTRITO) argumentos += " --sap-restrito";
-    if(!String.IsNullOrEmpty(regional)) argumentos += $" --{regional}";
-    ConsoleWrapper.Debug(Entidade.Executor, $"{cfg.SAP_SCRIPT} {argumentos}");
-    var proc = new System.Diagnostics.Process();
-    var startInfo = new System.Diagnostics.ProcessStartInfo
-      {
-        FileName = cfg.SAP_SCRIPT,
-        Arguments = argumentos,
-        UseShellExecute = false,
-        RedirectStandardOutput = true,
-        CreateNoWindow = true
-      };
-    proc.StartInfo = startInfo;
-    var linhas = new List<string>();
-    proc.Start();
-    var tempo = new System.Threading.Timer(state => {
-      if(!proc.HasExited)
-      {
-        var mos = Temporary.GetChildProcesses(proc);
-        foreach (var mo in mos)
-        {
-          mo.Kill();
-        }
-      }
-    }, null, cfg.ESPERA, Timeout.Infinite);
-    while (!proc.StandardOutput.EndOfStream)
-    {
-      var linha = proc.StandardOutput.ReadLine();
-      if(linha != null && linha != String.Empty) linhas.Add(linha);
-    }
-    tempo.Dispose();
-    proc.Dispose();
-    ConsoleWrapper.Debug(Entidade.Executor, String.Join('\n', linhas));
-    return linhas;
-  }
   private static IEnumerable<Process> GetChildProcesses(this Process process)
   {
     var children = new List<Process>();
@@ -79,26 +15,42 @@ public static class Temporary
     }
     return children;
   }
-  public static List<string> executar(List<string> listaValoresSeparadosPorTabulacao)
+  public static List<String> Executar(String aplicacao, String[] argumentos, Boolean expect_return)
   {
-    string textoValoresSeparadosPorTabulacao = string.Join("\n", listaValoresSeparadosPorTabulacao);
-    using(var proc = new System.Diagnostics.Process{
-      StartInfo = new System.Diagnostics.ProcessStartInfo
-        {
-          FileName = Configuration.GetInstance().IMG_SCRIPT,
-          Arguments = $"\"{textoValoresSeparadosPorTabulacao}\"",
-          UseShellExecute = false,
-          RedirectStandardOutput = true,
-          CreateNoWindow = true
-        }})
+    var linhas = new List<String>();
+    var cfg = Configuration.GetInstance();
+    var argumentos_texto = String.Join(' ', argumentos);
+    ConsoleWrapper.Debug(Entidade.Executor, aplicacao + argumentos_texto);
+    using var processo = new System.Diagnostics.Process();
+    var startInfo = new System.Diagnostics.ProcessStartInfo()
+    {
+      FileName = aplicacao,
+      Arguments = argumentos_texto,
+      UseShellExecute = !expect_return,
+      RedirectStandardOutput = expect_return,
+      CreateNoWindow = true
+    };
+    processo.StartInfo = startInfo;
+    processo.Start();
+    using var tempo = new System.Threading.Timer(state => {
+      if(!processo.HasExited)
       {
-        var linha = new List<string>();
-        proc.Start();
-        while (!proc.StandardOutput.EndOfStream)
+        var mos = GetChildProcesses(processo);
+        foreach (var mo in mos)
         {
-          linha.Add(proc.StandardOutput.ReadLine()!);
+          mo.Kill();
         }
-        return linha;
       }
+    }, null, cfg.ESPERA, Timeout.Infinite);
+    if(expect_return)
+    {
+      while (!processo.StandardOutput.EndOfStream)
+      {
+        var linha = processo.StandardOutput.ReadLine();
+        if(!String.IsNullOrEmpty(linha)) linhas.Add(linha);
+      }
+    }
+    ConsoleWrapper.Debug(Entidade.Executor, String.Join('\n', linhas));
+    return linhas;
   }
 }
