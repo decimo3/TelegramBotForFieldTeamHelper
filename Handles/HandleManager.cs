0fb84f7cb3f49edb396c43c8afcca6fa1f1d9c0d
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-08-21 11:57:04 -0300

    Added method to check user authorization

--- Handles/HandleManager.cs
+++ Handles/HandleManager.cs
@@ -86,4 +86,53 @@ public static class Manager
     }
     return;
   }
+  public async static Task<UsersModel?> HandleSecury(Int64 identificador, DateTime recebido_em)
+  {
+    var database = Database.GetInstance();
+    var telegram = HandleMessage.GetInstance();
+    //##################################################//
+    //       Verifica se o usuário está cadastrado      //
+    //       e se tem permissão para usar o sistema     //
+    //##################################################//
+    var user = database.RecuperarUsuario(identificador);
+    if(user is null)
+    {
+      database.InserirUsuario(new UsersModel() {
+        identifier = identificador,
+        create_at = recebido_em,
+        update_at = DateTime.Now
+      });
+      await telegram.sendTextMesssageWraper(identificador, "Seja bem vindo ao sistema de atendimento automático do chatbot!");
+      await telegram.sendTextMesssageWraper(identificador, "Eu não estou autorizado a te passar informações no momento");
+      await telegram.sendTextMesssageWraper(identificador, $"Seu identificador do Telegram é `{identificador}`.");
+      await telegram.sendTextMesssageWraper(identificador, "Informe ao seu supervisor esse identificador para ter acesso");
+      return null;
+    }
+    if(user.privilege == UsersModel.userLevel.desautorizar)
+    {
+      await telegram.sendTextMesssageWraper(identificador, "Eu não estou autorizado a te passar informações no momento");
+      await telegram.sendTextMesssageWraper(identificador, "Para restaurar o seu acesso ao sistema, solicite ao seu supervisor o acesso ao BOT.");
+      await telegram.sendTextMesssageWraper(identificador, $"Seu identificador do telegram é `{identificador}`, esse número deverá ser informado ao seu supervisor.");
+      return null;
+    }
+    //##################################################//
+    //       Verifica se o prazo da autorização de      //
+    //     eletricistas, supervisores e controladores   //
+    //##################################################//
+    if(user.dias_vencimento() <= 0)
+    {
+      await telegram.sendTextMesssageWraper(identificador, "Sua autorização expirou e não posso mais te passar informações");
+      await telegram.sendTextMesssageWraper(identificador, "Solicite a autorização novamente para o seu supervisor!");
+      await telegram.sendTextMesssageWraper(identificador, $"Seu identificador do Telegram é `{identificador}`.");
+      return null;
+    }
+    // Verifica se o cadastro está perto de expirar (7 dias antes) e avisa
+    if(user.dias_vencimento() <= 7)
+    {
+      await telegram.sendTextMesssageWraper(identificador, "Sua autorização está quase expirando!");
+      await telegram.sendTextMesssageWraper(identificador, "Solicite a **atualização** para o seu supervisor!");
+      await telegram.sendTextMesssageWraper(identificador, $"Seu identificador do Telegram é `{identificador}`.");
+    }
+    return user;
+  }
 }
