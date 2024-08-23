dd7cd5e3d4bdbc4b3f56587e485633d2279cd542
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-08-23 10:51:08 -0300

    Refactored response model to receive multiple values

--- Models/Response.cs
+++ Models/Response.cs
@@ -2,6 +2,11 @@ namespace telbot.API;
 public class Response
 {
   public Int32 status { get; set; }
-  public String? message { get; set; }
-  public List<String>? table { get; set; }
+  public List<Entities> entities { get; set; } = new();
 }
+public class Entities
+{
+  public typeEntity type { get; set; }
+  public String data { get; set; }
+}
+public enum typeEntity {TXT, PIC, XLS, PDF, XYZ}
