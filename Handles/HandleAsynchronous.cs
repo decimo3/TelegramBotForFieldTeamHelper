0b6c961c01acd77e50d15b48bb7613993802a641
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-08-23 09:24:10 -0300

    Removed unnecessary model class

--- Handles/HandleAsynchronous.cs
+++ Handles/HandleAsynchronous.cs
@@ -3,21 +3,21 @@ using telbot.models;
 namespace telbot.handle;
 public static class HandleAsynchronous
 {
-  // TODO - Recebe, verifica e registra no banco de dados
-  public static async Task Soiree(RequestText solicitacao)
+  // DONE - Recebe, verifica e registra no banco de dados
+  public static async Task Soiree(Int64 identificador, String mensagem, DateTime received_at)
   {
     var database = Database.GetInstance();
     var telegram = HandleMessage.GetInstance();
-    ConsoleWrapper.Write(Entidade.Usuario, $"{solicitacao.Identificador} escreveu: {solicitacao.Mensagem}");
-    var request = Validador.isRequest(solicitacao.Mensagem);
+    ConsoleWrapper.Write(Entidade.Usuario, $"{identificador} escreveu: {mensagem}");
+    var request = Validador.isRequest(mensagem);
     if (request is null)
     {
-      await telegram.sendTextMesssageWraper(solicitacao.Identificador, "Verifique o formato da informação e tente novamente da forma correta!");
-      await telegram.sendTextMesssageWraper(solicitacao.Identificador, "Se tiver em dúvida de como usar o bot, digite /ajuda.");
+      await telegram.sendTextMesssageWraper(identificador, "Verifique o formato da informação e tente novamente da forma correta!");
+      await telegram.sendTextMesssageWraper(identificador, "Se tiver em dúvida de como usar o bot, digite /ajuda.");
       return;
     }
-    request.received_at = solicitacao.RecebidoEm;
-    request.identifier = solicitacao.Identificador;
+    request.identifier = identificador;
+    request.received_at = received_at.ToLocalTime();
     database.InserirSolicitacao(request);
   }
   // TODO - Coleta do banco de dados e realiza a solicitação
