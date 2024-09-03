74bbdaf0829b68e92c9d5e21c4cc4b118368c879
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-09-03 09:05:22 -0300

    Simplified subsystem response model

--- Models/Response.cs
+++ Models/Response.cs
@@ -2,11 +2,5 @@ namespace telbot.API;
 public class Response
 {
   public Int32 status { get; set; }
-  public List<Entities> entities { get; set; } = new();
-}
-public class Entities
-{
-  public typeEntity type { get; set; }
   public String data { get; set; }
 }
-public enum typeEntity {TXT, PIC, XLS, PDF, XYZ}
