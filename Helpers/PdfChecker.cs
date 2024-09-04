fc1a6e5fb812aa70ed79f02a129c9f7298be222d
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-09-04 11:14:36 -0300

    Method to get instalation from PDF Corrected

--- Helpers/PdfChecker.cs
+++ Helpers/PdfChecker.cs
@@ -3,16 +3,22 @@ public static partial class PdfHandle
 {
   public static Int64 Check(String filepath)
   {
-    var text = new System.Text.StringBuilder();
     using var reader = new iTextSharp.text.pdf.PdfReader(filepath);
+    var re = new System.Text.RegularExpressions.Regex("^[0-9]{10}$");
     var strategy = new iTextSharp.text.pdf.parser.SimpleTextExtractionStrategy();
     for (var page = 1; page <= reader.NumberOfPages; page++)
     {
         var currentText = iTextSharp.text.pdf.parser.PdfTextExtractor.GetTextFromPage(reader, page, strategy);
-        text.Append(currentText);
+        var lines = currentText.Split('\n');
+        foreach (var line in lines)
+        {
+          var match = re.Match(line);
+          if(match.Success)
+          {
+            return Int64.Parse(match.Value);
+          }
+        }
     }
-    var re = new System.Text.RegularExpressions.Regex("^[0-9]{10}$");
-    var match = re.Match(text.ToString());
-    return match.Success ? Convert.ToInt64(match.Value) : 0;
-    }
+    return 0;
+  }
 }
