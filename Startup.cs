666b07e205e93f2624736531ad26e8ece94b9d92
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-09-09 10:58:37 -0300

    Implemented username collection when receiving user contact

--- Startup.cs
+++ Startup.cs
@@ -140,7 +140,8 @@ class Startup
         }
         await HandleTypeMessage.PhoneNumberType(
           usuario: usuario,
-          telefone: Convert.ToInt64(update.Message.Contact.PhoneNumber.Replace("+", ""))
+          telefone: Convert.ToInt64(update.Message.Contact.PhoneNumber.Replace("+", "")),
+          username: update.Message.From.FirstName + " " + update.Message.From.LastName
         );
         break;
       }
