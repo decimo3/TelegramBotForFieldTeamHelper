2da62a256d5b1f1132043a07f105c08633e675b3
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2024-09-20 08:40:59 -0300

    Added model 'ZoneModel' to receive zone location info

---
+++ Models/ZoneModel.cs
@@ -0,0 +1,9 @@
+namespace telbot.models;
+public class ZoneModel
+{
+  public String Nome { get; set; } = String.Empty;
+  public String Tipo { get; set; } = String.Empty;
+  public Double Lat { get; set; } = 0;
+  public Double Lon { get; set; } = 0;
+  public Double Mts { get; set; } = 0;
+}
