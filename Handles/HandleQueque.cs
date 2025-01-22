285f03c5e2226f5f63e72244e5f74f337f306b51
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-01-22 14:45:30 -0300

    Added singleton class to handle with request queque in memory instead making database query

---
+++ Handles/HandleQueque.cs
@@ -0,0 +1,65 @@
+using System.Data.Entity.Core;
+using System.Linq.Expressions;
+using telbot.models;
+namespace telbot.handle;
+public class HandleQueQue
+{
+  private static readonly List<logsModel> lista = new();
+  private static HandleQueQue? _instance = null;
+  private HandleQueQue() {}
+  public static HandleQueQue GetInstance()
+  {
+    lock(lista)
+    {
+      _instance ??= new();
+      return _instance;
+    }
+  }
+  public List<logsModel> Get()
+  {
+    lock(lista)
+    {
+      return new List<logsModel>(lista);
+    }
+  }
+  public List<logsModel>? Get(Expression<Func<logsModel, bool>> expression)
+  {
+    if (expression is null)
+      throw new ArgumentNullException(nameof(expression));
+    lock(lista)
+    {
+      return lista.Where(expression.Compile()).ToList();
+    }
+  }
+  public void Add(logsModel request)
+  {
+    if (request == null)
+      throw new ArgumentNullException(nameof(request));
+    lock(lista)
+    {
+      lista.Add(request);
+    }
+  }
+  public void Del(logsModel request)
+  {
+    if (request == null)
+      throw new ArgumentNullException(nameof(request));
+    lock(lista)
+    {
+      if(!lista.Remove(request))
+        throw new ObjectNotFoundException(nameof(request));
+    }
+  }
+  public void Alt(logsModel request)
+  {
+    if (request == null)
+      throw new ArgumentNullException(nameof(request));
+    lock(lista)
+    {
+      var item = lista.Find(l => l.rowid == request.rowid) ??
+        throw new ObjectNotFoundException(nameof(request));
+      lista.Remove(item);
+      lista.Add(request);
+    }
+  }
+}
