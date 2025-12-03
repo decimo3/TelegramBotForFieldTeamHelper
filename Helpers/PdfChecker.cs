4ce32a756112b2c3945ea8a5d7d10745f824b893
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2025-12-03 14:52:51 -0300

    fix: change regex to dot style number, due changes on PDF invoice

--- Helpers/PdfChecker.cs
+++ Helpers/PdfChecker.cs
@@ -1,10 +1,12 @@
+using System.Text.RegularExpressions;
 namespace telbot.Helpers;
 public partial class PdfHandle
 {
+  private const string CPF_REGEX = @"\d{1,3}(?:\.\d{3}){2}\.\d{3}-\d{2}";
   private static Int64 Check(String filepath)
   {
     using var reader = new iTextSharp.text.pdf.PdfReader(filepath);
-    var re = new System.Text.RegularExpressions.Regex("^[0-9]{10}$");
+    var re = new Regex(CPF_REGEX);
     var strategy = new iTextSharp.text.pdf.parser.SimpleTextExtractionStrategy();
     for (var page = 1; page <= reader.NumberOfPages; page++)
     {
@@ -15,7 +17,8 @@ public partial class PdfHandle
           var match = re.Match(line);
           if(match.Success)
           {
-            return Int64.Parse(match.Value);
+            // Clear output removing dots and dashes
+            return Int64.Parse(Regex.Replace(match.Value, @"\D", ""));
           }
         }
     }
