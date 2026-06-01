500cc67a7fbcd9f3d0c9257a366e3cf9a9961876
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-06-01 15:12:10 -0300

    refactor: reduce conditional complexity in expiration check

--- Models/UsersModel.cs
+++ Models/UsersModel.cs
@@ -3,6 +3,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace telbot;
 public class UsersModel : IValidatableObject
 {
+  private const int PRAZO_MAX = 360;
+  private const int PRAZO_MID = 180;
+  private const int PRAZO_MIN = 90;
   [Key]
   [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
   public Int64 rowid { get; set; }
@@ -74,20 +77,17 @@ public class UsersModel : IValidatableObject
   }
   public Int32 dias_vencimento()
   {
-    if(this.privilege == userLevel.eletricista)
-      return (this.update_at.AddDays(30) - DateTime.Now).Days;
-    if(this.privilege == userLevel.controlador)
-      return (this.update_at.AddDays(30) - DateTime.Now).Days;
-    if(this.privilege == userLevel.supervisor)
-      return (this.update_at.AddDays(90) - DateTime.Now).Days;
-    if(this.privilege == userLevel.comunicador)
-      return 99;
-    if(this.privilege == userLevel.administrador)
-      return 99;
-    if(this.privilege == userLevel.coordenador)
-      return 99;
-    if(this.privilege == userLevel.proprietario)
-      return 99;
+    if (this.privilege is userLevel.eletricista or userLevel.controlador)
+      return (this.update_at.AddDays(PRAZO_MIN) - DateTime.Now).Days;
+
+    if (this.privilege is userLevel.supervisor or userLevel.comunicador)
+      return (this.update_at.AddDays(PRAZO_MID) - DateTime.Now).Days;
+
+    if (this.privilege is userLevel.administrador or userLevel.coordenador)
+      return (this.update_at.AddDays(PRAZO_MAX) - DateTime.Now).Days;
+
+    if (this.privilege is userLevel.proprietario)
+      return int.MaxValue;
     return -1;
   }
 }
