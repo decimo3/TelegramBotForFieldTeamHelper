f7ad859a4413b637b2eb3db7870e004cc2bbd761
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-06-01 07:59:37 -0300

    refactor: resolve document directory nullable warning with non-null assertion

--- Helpers/DocHandler.cs
+++ Helpers/DocHandler.cs
@@ -47,7 +47,7 @@ public static class DocHandler
           messageId = messageId,
           identifier = identifier,
           filename = docInfoFileName,
-          parent = docInfo.Directory?.Name,
+          parent = docInfo.Directory!.Name,
           updatedAt = docInfo.LastWriteTimeUtc.ToLocalTime()
         });
         continue;
